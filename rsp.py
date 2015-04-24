31fac877a2d1d33e76bccf4b50899e59bf841255
John Hammink <john.hammink@gmail.com>
2015-04-24 00:07:55 -0700

    Create rsp.py
    Example 1:  A single-handed game of rock-paper-scissors

---
+++ rsp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#a single hand game of rock, paper, scissors
+
+import random
+
+tie= "a tie"
+p1 = "Player 1; Player 2 loses"
+p2 = "Player 2; Player 1 loses"
+
+myDict = {('rock', 'rock') : tie, ('rock', 'paper') : p2,
+('rock', 'scissors') : p1, ('paper', 'rock') : p1, ('paper', 'paper') : tie, ('paper', 'scissors') : p2, ('scissors', 'rock') : p2, ('scissors', 'paper') : p1, ('scissors', 'scissors') : tie } 
+
+def throw(player1, player2):
+    verdict = myDict[(player1, player2)]
+    print "The game goes to " + verdict
+
+print "Ya wanna play Rock, Paper, Scissors?"
+player1 = raw_input("Throw!  Choose 'rock', 'paper', or 'scissors': " )
+print "Player 1 chooses: " + player1
+player2 = random.choice(['rock', 'paper', 'scissors'])
+print "Player 2 chooses: " + player2
+throw(player1, player2)
